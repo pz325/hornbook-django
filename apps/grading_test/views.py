5a4303b8990611c45327c759ef090f5dcf148197
Ping Zou <sg71.cherub@gmail.com>
2013-09-26 23:03:03 +0100

    start grading_test app

--- apps/grading_test/views.py
+++ apps/grading_test/views.py
@@ -1,10 +1,9 @@
 # Create your views here.
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response
+from django.template import RequestContext
 
 @login_required
 def index(request):
-    '''
-    Test page for horn_api app
-    '''
-    return render_to_response('grading_test/index.html')
+    return render_to_response('grading_test/index.html', 
+        context_instance=RequestContext(request))
