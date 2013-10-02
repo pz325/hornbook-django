8e7588387885673442f00dcd43f6bd8ab4478b42
Ping Zou <sg71.cherub@gmail.com>
2013-10-03 00:16:33 +0100

    update method comments

--- static/js/study.js
+++ static/js/study.js
@@ -2,6 +2,10 @@ var API_SAVE_STUDY_URL = "/study/save_study/";
 var API_GET_STUDY_BWTEEN_URL = "/study/get_study_between";
 
 var StudyAPI = (function() {
+    /**
+     * @param start_date "10/02/2013"
+     * @param end_date "10/03/2013"
+     */
     var get_study_between = function(start_date, end_date) {
         console.log('start_date: ', start_date);
         console.log('end_date: ', end_date);
@@ -16,6 +20,9 @@ var StudyAPI = (function() {
         return promise;
     };
 
+    /**
+     * @param vocabularies "u0x8fd9 u0x90a3 u0x6765"
+     */
     var save_study = function(vocabularies) {
         var promise = $.ajax({
             type: "POST",
