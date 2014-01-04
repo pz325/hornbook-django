f50490cd25eca77face85fd532c315b48d3d2436
Ping Zou <sg71.cherub@gmail.com>
2014-01-04 19:20:10 +0000

    add card style to han_character div

--- apps/dashboard/models.py
+++ apps/dashboard/models.py
@@ -4,11 +4,11 @@ from django.contrib.auth.models import User
 from django.contrib import admin
 
 # Create your models here.
-class UserVocabulary(models.Model):
+class UserVocabularyRecord(models.Model):
     user = models.ForeignKey(User, editable=False)
     date_stamp = models.DateTimeField(auto_now=False)
     num_new_vocabulary = models.PositiveSmallIntegerField(default=0)
     num_learning_vocabulary = models.PositiveSmallIntegerField(default=0)
     num_grasped_vocabulary = models.PositiveSmallIntegerField(default=0)
 
-admin.site.register(UserVocabulary)
+admin.site.register(UserVocabularyRecord)
--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -10,7 +10,61 @@ from django.core.serializers.json import DjangoJSONEncoder
 import datetime
 import json
 
+from django.contrib.auth.models import User
+from models import UserVocabularyRecord
+from apps.study.models import StudyHistory
+
 @login_required
 def index(request):
     return render_to_response('dashboard/index.html', 
         context_instance=RequestContext(request))
+
+@login_required
+def get_user_vocabulary_record(request):
+    '''
+    HTTP GET /dashboard/get_user_vocabular_record?date_from&date_to&period
+
+    Get user's vocabulary record, from date_from to date_to
+    
+    @param date_from string of datetime, in the format of %m/%d/%Y
+    @param date_to string of datetime, in the format of %m/%d/%Y
+    @param period enum {last_week, last_month_, last_3_month, last_half_year, last_year, all}
+    @return JSON string
+        {
+            'start': date_from,
+            'end': date_to,
+            'period': period,
+            'data': [             // a list of results
+                {
+                    'date_stamp': date_stamp,
+                    'num_new_vocabulary': num_new_vocabulary,
+                    'num_learning_vocabulary': num_learning_vocabulary,
+                    'num_grasped_vocabulary': num_grasped_vocabulary
+                },
+                {
+                    ...
+                }
+            ]
+        }
+    '''
+    return HttpResponse(json.dumps(ret, cls=DjangoJSONEncoder))
+
+def get_user_vocabulary_record_(user, date_from, date_to):
+    '''
+    Get user's vocabulary records from date_from to date_to (record on date_to is included)
+    
+    @param user type of django.contrib.auth.models.User
+    @param date_from type of datetime.datetime
+    @param date_to type of datetime.datetime
+    @return list of UserVocabularyRecord instances
+    '''
+    pass
+
+def update_user_vocabulary_record(user, date_from):
+    '''
+    Update user's vocabulary record since date_from
+
+    @param user type of django.contrib.auth.models.User
+    @param date_from type of datetime.datetime
+    '''
+    pass
