ea07d4dfb89d5d2a26604f7ae854ac6e769ae27e
Ping Zou <sg71.cherub@gmail.com>
2014-01-18 16:37:14 +0000

    show most_common statistics

--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -11,6 +11,7 @@ from django.core.exceptions import ObjectDoesNotExist
 
 import datetime
 import json
+import logging
 
 from django.contrib.auth.models import User
 from models import UserVocabularyRecord
@@ -18,7 +19,10 @@ from apps.study.models import StudyHistory
 
 @login_required
 def index(request):
-    statistics = get_statistics_(request.user)
+    ret1 = get_study_history_statistics(request.user)
+    ret2 = get_most_common_statistics(request.user)
+    ret1.update(ret2)
+    statistics = ret1
     return render(request, 
         'dashboard/index.html', 
         {"statistics": statistics})
@@ -34,16 +38,41 @@ def get_statistics(request):
             num_grasped: 5
         }
     '''
-    ret = get_statistics_(request.user)
+    ret = get_study_history_statistics(request.user)
     return HttpResponse(json.dumps(ret, cls=DjangoJSONEncoder))
 
-def get_statistics_(user):
+def get_study_history_statistics(user):
     new_v = [h.vocabulary for h in StudyHistory.objects.filter(user=user, history_type='N')]
     studying_v = [h.vocabulary for h in StudyHistory.objects.filter(user=user, history_type='S')]
     grasped_v = [h.vocabulary for h in StudyHistory.objects.filter(user=user, history_type='G')]
     ret = dict(num_new=len(new_v),
         num_studying=len(studying_v),
         num_grasped=len(grasped_v))
+    return ret
+
+
+from apps.hornbook_api.models import MostCommonCharacter
+
+def get_most_common_statistics(user):
+    v = []
+    for h in StudyHistory.objects.filter(user=user):
+        if len(h.vocabulary) > 1:
+            for c in h.vocabulary:
+                v.append(c)
+        else:
+            v.append(h.vocabulary)
+    
+    v = set(v)
+    common = [c for (c, f) in MostCommonCharacter.get_all()]
+    in_common = 0
+    out_common = 0
+    for h in v:
+        if h in common:
+            in_common += 1
+        else:
+            out_common += 1
+    ret = dict(in_common=in_common, 
+        out_common=out_common)
     return ret
 
 def update_record(user, num_new, num_studying, num_grasped):
