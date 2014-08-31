4d9ddf9690cdb4aca9e838ad0c00f07a40e23aad
Ping Zou <sg71.cherub@gmail.com>
2014-08-31 21:30:34 +0100

    add /leitner/get log

--- apps/leitner/views.py
+++ apps/leitner/views.py
@@ -58,16 +58,56 @@ def get(request):
     logging.info('sesson count: {session_count} -> session deck id: {session_deck_id}'.format(session_count=session_count.count, session_deck_id=session_deck_id))
 
     current_deck = [h.hanzi for h in Leitner.objects.filter(user=request.user, deck='C')]
-    #level1_deck = []#[h.hanzi for h in Leitner.objects.filter(user=request.user, deck=str(session_deck_id), level=1)]
     level1_deck = [h.hanzi for h in Leitner.objects.filter(user=request.user, deck=str((session_deck_id+2)%10), level=1)]
     level2_deck = [h.hanzi for h in Leitner.objects.filter(user=request.user, deck=str((session_deck_id+5)%10), level=2)]
     level3_deck = [h.hanzi for h in Leitner.objects.filter(user=request.user, deck=str((session_deck_id-1)%10), level=3)]
     retired_deck = [h.hanzi for h in Leitner.objects.filter(user=request.user, deck='R')]
     permanent_deck = [h.hanzi for h in Leitner.objects.filter(user=request.user, deck='P')]
+
+    # logging
+    log_get_result(current_deck, level1_deck, level2_deck, level3_deck, retired_deck, permanent_deck)
+    all_deck = Leitner.objects.filter(user=request.user);
+    log_all(all_deck)
+
     random.shuffle(retired_deck)
     random.shuffle(permanent_deck)
     ret = current_deck + level1_deck + level2_deck + level3_deck + retired_deck[:7] + permanent_deck[:3]
     return HttpResponse(json.dumps(ret, cls=DjangoJSONEncoder))
+
+
+def log_get_result(current_deck, level1_deck, level2_deck, level3_deck, retired_deck, permanent_deck):
+    log_str = '\n/leitner/get result\n'
+    log_str += '{n} from Current Deck\n'.format(n=len(current_deck))
+    log_str += '{n} from Level 1 Deck\n'.format(n=len(level1_deck))
+    log_str += '{n} from Level 2 Deck\n'.format(n=len(level2_deck))
+    log_str += '{n} from Level 3 Deck\n'.format(n=len(level3_deck))
+    log_str += '{n} from Retired Deck\n'.format(n=len(retired_deck))
+    log_str += '{n} from Permanent Deck\n'.format(n=len(permanent_deck))
+    logging.info(log_str)
+
+def log_all(all_deck):
+    decks = {'C': [], '1': {}, '2': {}, '3': {}, 'R': [], 'P': []}
+    for j in range(1, 4):
+        for i in range(0, 10):
+            decks[str(j)][str(i)] = []
+    for h in all_deck:
+        if h.deck == 'C' or h.deck == 'R' or h.deck == 'P':
+            decks[h.deck].append(h.hanzi)
+        elif h.level == 1 or h.level == 2 or h.level == 3:
+            decks[str(h.level)][h.deck].append(h.hanzi)
+        else:
+            logging.info('missing: {hanzi}'.format(hanzi=h.hanzi))
+    log_str = 'ALL:\n'
+    log_str += '{n} in Current Deck:\n'.format(n=len(decks['C']), deck_C=decks['C'])
+    for j in range(1, 4):
+        log_str += 'Level {j}:\n'.format(j=j)
+        for i in range(0, 10):
+            log_str += '    {n} in Deck {i}:\n'.format(n=len(decks[str(j)][str(i)]), i=i, deck_i=decks[str(j)][str(i)])
+    log_str += '{n} in Retired Deck:\n'.format(n=len(decks['R']), deck_R=decks['R'])
+    log_str += '{n} in Permanent Deck:\n'.format(n=len(decks['P']), deck_P=decks['P'])
+    logging.info(log_str)
+    
+
 
 @login_required
 def count(request):
